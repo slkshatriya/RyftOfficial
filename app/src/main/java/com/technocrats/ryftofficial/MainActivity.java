dded4bb331ae0e8b91e9d1bf149f3249e78e3146
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2021-02-19 18:40:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/technocrats/ryftofficial/MainActivity.java
+++ app/src/main/java/com/technocrats/ryftofficial/MainActivity.java
@@ -9,6 +9,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,14 +25,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
     private  static int RC_SIGN_IN=100;
     FirebaseAuth mAuth;
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+    private static final String EMAIL = "email";
 
 
     @Override
@@ -34,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth=FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
-
+        loginButton=findViewById(R.id.facebookLoginInButton);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("831473135278-01642e3pthvh8kn13jr8jf7o8cbd42or.apps.googleusercontent.com")
                 .requestEmail()
@@ -51,7 +65,48 @@ public class MainActivity extends AppCompatActivity {
                 signIn();
             }
         });
+
+        InitializeFacebook();
+
     }
+
+    private void InitializeFacebook() {
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        facebookHandlerCode(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+
+    private void facebookHandlerCode(AccessToken accessToken)
+    {
+        AuthCredential credential= FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
