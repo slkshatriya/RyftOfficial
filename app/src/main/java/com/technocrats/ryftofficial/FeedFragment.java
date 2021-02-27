ed82a8518e04afab48b6ec5f2c15d9bb68ad7407
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2021-02-27 17:10:04 +0530

    Update FeedFragment.java

--- app/src/main/java/com/technocrats/ryftofficial/FeedFragment.java
+++ app/src/main/java/com/technocrats/ryftofficial/FeedFragment.java
@@ -1,22 +1,30 @@
 package com.technocrats.ryftofficial;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FeedFragment extends Fragment {
-
+    List<Model> ModelList=new ArrayList<>();
     RecyclerView mRecyclerView;
-    RecyclerView.LayoutManager layoutManager;
     CustomAdapter adapter;
     View view;
 
@@ -26,18 +34,18 @@ public class FeedFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_feed, container, false);
 
+        setProdItemRecycler(ModelList);
 
-        List<Model> productsList = new ArrayList<>();
-        setProdItemRecycler(productsList);
         FirebaseDatabase.getInstance().getReference().child("posts").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-                Model model=new Model(snapshot.child("tech used 1").getValue().toString(),
-                        snapshot.child("tech used 2").getValue().toString(),
-                        snapshot.child("title").getValue().toString(),
-                        snapshot.child("projectImageUrl").getValue().toString());
-                productsList.add(model);
+                String techUsed1=snapshot.child("tech used 1").getValue().toString();
+                String techUsed2=snapshot.child("tech used 2").getValue().toString();
+                String title=snapshot.child("title").getValue().toString();
+                String Url=snapshot.child("projectImageUrl").getValue().toString();
+                Model model=new Model(techUsed1,techUsed2,title,Url);
+                ModelList.add(model);
                 adapter.notifyDataSetChanged();
             }
 
@@ -62,11 +70,9 @@ public class FeedFragment extends Fragment {
             }
         });
 
-        
 
         return view;
     }
-
     private void setProdItemRecycler(List<Model> productsList){
 
         mRecyclerView= view.findViewById(R.id.recyclerView);
