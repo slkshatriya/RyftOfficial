4db128e31ddaa9e2745e373df50daeee16a6bbc9
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2021-03-01 12:55:17 +0530

    Update DevFragment.java

--- app/src/main/java/com/technocrats/ryftofficial/DevFragment.java
+++ app/src/main/java/com/technocrats/ryftofficial/DevFragment.java
@@ -13,7 +13,9 @@ import androidx.fragment.app.Fragment;
 
 
 public class DevFragment extends Fragment {
-    TextView techUsed2TextView,techUsed1TextView,titleTextView;
+    TextView techUsed2TextView,techUsed1TextView,
+            titleTextView,techUsed3TextView,techUsed4TextView,
+            step1TextView,step2TextView,step3TextView,step4TextView;
     Intent intent;
 
     @Override
@@ -30,9 +32,21 @@ public class DevFragment extends Fragment {
         titleTextView=getView().findViewById(R.id.ProjectTitle);
         techUsed1TextView=getView().findViewById(R.id.techUsed1);
         techUsed2TextView=getView().findViewById(R.id.techUsed2);
+        techUsed3TextView=getView().findViewById(R.id.techUsed3);
+        techUsed4TextView=getView().findViewById(R.id.techUsed4);
+        step1TextView=getView().findViewById(R.id.longDescription);
+        step2TextView=getView().findViewById(R.id.longDescription1);
+        step3TextView=getView().findViewById(R.id.longDescription2);
+        step4TextView=getView().findViewById(R.id.longDescription3);
 
+        step1TextView.setText(intent.getExtras().getString("step 1"));
+        step2TextView.setText(intent.getExtras().getString("step 2"));
+        step3TextView.setText(intent.getExtras().getString("step 3"));
+        step4TextView.setText(intent.getExtras().getString("step 4"));
         titleTextView.setText(intent.getExtras().getString("title"));
         techUsed1TextView.setText(intent.getExtras().getString("tech used 1"));
         techUsed2TextView.setText(intent.getExtras().getString("tech used 2"));
+        techUsed3TextView.setText(intent.getExtras().getString("tech used 3"));
+        techUsed4TextView.setText(intent.getExtras().getString("tech used 4"));
     }
 }
