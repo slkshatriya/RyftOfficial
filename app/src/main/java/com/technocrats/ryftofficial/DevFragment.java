15a305688780eefc877ce2e93ad8f518ec1e8710
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2021-03-03 17:12:08 +0530

    Update DevFragment.java

--- app/src/main/java/com/technocrats/ryftofficial/DevFragment.java
+++ app/src/main/java/com/technocrats/ryftofficial/DevFragment.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 
 public class DevFragment extends Fragment {
     TextView techUsed2TextView,techUsed1TextView,
             titleTextView,techUsed3TextView,techUsed4TextView,
             step1TextView,step2TextView,step3TextView,step4TextView;
+    EditText submitLinkEdittext;
     Intent intent;
+    Button submitButton,requestCertificateButton;
+    String username,evaluated;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +39,7 @@ public class DevFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dev, container, false);
     }
 
-   /* @Override
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         titleTextView=getView().findViewById(R.id.ProjectTitle);
@@ -36,6 +51,79 @@ public class DevFragment extends Fragment {
         step2TextView=getView().findViewById(R.id.longDescription1);
         step3TextView=getView().findViewById(R.id.longDescription2);
         step4TextView=getView().findViewById(R.id.longDescription3);
+        submitButton=getView().findViewById(R.id.cSubmit);
+        requestCertificateButton=getView().findViewById(R.id.rCert);
+        submitLinkEdittext=getView().findViewById(R.id.submitCodeLink);
+        final String projectId=intent.getExtras().getString("projectId");
+        username="username";
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link=submitLinkEdittext.getText().toString();
+                if(link.equals(""))
+                {
+                    Toast.makeText(getContext(),"Please Provide a link"
+                            ,Toast.LENGTH_SHORT).show();
+                }
+                else
+                    {
+                        HashMap<String,String> submission=new HashMap<>();
+                        submission.put("link",link);
+                        submission.put("username",username);
+                        FirebaseDatabase.getInstance().getReference().child("submissions")
+                        .child(projectId).push().setValue(submission);
+                        Toast.makeText(getContext(),"Link Submitted",Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
+
+        requestCertificateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                    FirebaseDatabase.getInstance().getReference().child("submissions").
+                            child(projectId).addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            String user=snapshot.child("username").toString();
+                            try {
+                                evaluated = snapshot.child("evaluated").toString();
+                            } catch (Exception e)
+                            {
+                                evaluated=null;
+                            }
+                            if(user.equals(username)&& evaluated != null)
+                            {   //if user is certified
+                                Toast.makeText(getContext(),"you are certified",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+            }
+        });
+
 
         step1TextView.setText(intent.getExtras().getString("step 1"));
         step2TextView.setText(intent.getExtras().getString("step 2"));
@@ -46,5 +134,5 @@ public class DevFragment extends Fragment {
         techUsed2TextView.setText(intent.getExtras().getString("tech used 2"));
         techUsed3TextView.setText(intent.getExtras().getString("tech used 3"));
         techUsed4TextView.setText(intent.getExtras().getString("tech used 4"));
-    } */
+    }
 }
