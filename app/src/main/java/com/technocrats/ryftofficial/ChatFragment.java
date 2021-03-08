7e9be29514a02992841f2cbf12593e324514eb47
krishna chaitanya <76201176+krishnachaitanya0107@users.noreply.github.com>
2021-03-08 15:34:57 +0530

    Update ChatFragment.java

--- app/src/main/java/com/technocrats/ryftofficial/ChatFragment.java
+++ app/src/main/java/com/technocrats/ryftofficial/ChatFragment.java
@@ -5,15 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-
-import androidx.fragment.app.Fragment;
 
 public class ChatFragment extends Fragment {
     ArrayAdapter arrayAdapter;
     ArrayList<String> groups;
     ArrayList<String> keys;
+    String userId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +36,7 @@ public class ChatFragment extends Fragment {
         return view;
     }
 
-   /* @Override
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         groups=new ArrayList<String>();
@@ -34,10 +46,40 @@ public class ChatFragment extends Fragment {
         listView.setAdapter(arrayAdapter);
         FirebaseAuth mauth=FirebaseAuth.getInstance();
         FirebaseUser user=mauth.getCurrentUser();
-        final String userEmail=user.getEmail();
+        if (user != null) {
+            userId=user.getUid();
+        }
+        if(userId!=null) {
+            FirebaseDatabase.getInstance().getReference().child("users")
+                    .child(userId).child("enrolled groups")
+                    .addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            groups.add(snapshot.getValue().toString());
+                            arrayAdapter.notifyDataSetChanged();
+                        }
 
-        //get list of enrolled chat groups from firebase
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
         
-    }*/
+    }
 }
